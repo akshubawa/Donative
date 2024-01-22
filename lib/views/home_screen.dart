a04f6b2d66bb09cfe967fe29428887d75754ae1e
Kabir-Yadav <kabirrao1@gmail.com>
2024-01-22 22:48:46 +0530

    ui changes

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,60 +10,75 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Crowd Funding App'),
       ),
-      body: Stack(
-        alignment: AlignmentDirectional.center,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Positioned(
-                top: 300,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Search...',
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
+          Positioned(
+            top: 300,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search...',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
                     ),
-                    IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        // Handle search action
-                      },
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 200,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    buildFundingCard(
-                      title: 'Cancer Funding',
-                      initiator: 'XYZ Organization',
-                      imageUrl: 'lib/app/assets/cancer_image.webp',
-                    ),
-                    buildFundingCard(
-                      title: 'Liver Transplant Funding',
-                      initiator: 'ABC Foundation',
-                      imageUrl: 'lib/app/assets/liver_transplant_image.jpg',
-                    ),
-                    // Add more cards as needed
-                  ],
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    // Handle search action
+                  },
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 570,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                buildFundingCard(
+                    title: 'Cancer Funding',
+                    initiator: 'XYZ Organization',
+                    imageUrl: 'lib/app/assets/cancer_image.webp',
+                    raisedAmount: 500,
+                    totalAmount: 1000),
+                buildFundingCard(
+                    title: 'Liver Transplant Funding',
+                    initiator: 'ABC Foundation',
+                    imageUrl: 'lib/app/assets/liver_transplant_image.jpg',
+                    raisedAmount: 500,
+                    totalAmount: 1000),
+                buildFundingCard(
+                    title: 'Liver Transplant Funding',
+                    initiator: 'ABC Foundation',
+                    imageUrl: 'lib/app/assets/liver_transplant_image.jpg',
+                    raisedAmount: 500,
+                    totalAmount: 1000),
+                buildFundingCard(
+                    title: 'Liver Transplant Funding',
+                    initiator: 'ABC Foundation',
+                    imageUrl: 'lib/app/assets/liver_transplant_image.jpg',
+                    raisedAmount: 500,
+                    totalAmount: 1000),
+                buildFundingCard(
+                    title: 'Liver Transplant Funding',
+                    initiator: 'ABC Foundation',
+                    imageUrl: 'lib/app/assets/liver_transplant_image.jpg',
+                    raisedAmount: 500,
+                    totalAmount: 1000),
+                // Add more cards as needed
+              ],
+            ),
           ),
         ],
       ),
@@ -73,9 +89,15 @@ class HomeScreen extends StatelessWidget {
     required String title,
     required String initiator,
     required String imageUrl,
+    required double raisedAmount,
+    required double totalAmount,
   }) {
+    // Calculate the percentage of money raised
+    double progressPercentage = ((raisedAmount / totalAmount) * 100);
+
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding:
+          const EdgeInsets.only(top: 8.0, bottom: 5.0, left: 8.0, right: 20),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
@@ -92,7 +114,7 @@ class HomeScreen extends StatelessWidget {
               child: Image.asset(
                 imageUrl,
                 height: 100,
-                width: 220,
+                width: double.maxFinite,
                 fit: BoxFit.cover,
               ),
             ),
@@ -115,6 +137,31 @@ class HomeScreen extends StatelessWidget {
                       color: Colors.grey,
                     ),
                   ),
+                  SizedBox(height: 4),
+
+                  // LinearProgressIndicator for fundraising progress
+                  Row(
+                    children: [
+                      SizedBox(width: 264.7),
+                      Text(
+                        'Total: \$${totalAmount.toInt()}',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  // Stack for combining progress bar and raised amount
+                  FAProgressBar(
+                    currentValue: progressPercentage,
+                    displayText: '%',
+                    animatedDuration: const Duration(milliseconds: 800),
+                    backgroundColor: Colors.white,
+                    progressColor: Color.fromARGB(255, 103, 103, 103),
+                    size: 20,
+                  )
                 ],
               ),
             ),
